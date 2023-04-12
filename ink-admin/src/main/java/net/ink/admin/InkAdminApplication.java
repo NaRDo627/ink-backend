ce8b92eb1ccc0b6acbdc571186e0407e7a1887ca
hkpark02 <hkpark02@cafe24corp.com>
2023-04-12 22:16:47 +0900

    Admin 모듈 추가

---
+++ ink-admin/src/main/java/net/ink/admin/InkAdminApplication.java
@@ -0,0 +1,13 @@
+package net.ink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkAdminApplication.class, args);
+    }
+
+}
---
+++ ink-admin/src/test/java/net/ink/admin/InkAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ink.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
